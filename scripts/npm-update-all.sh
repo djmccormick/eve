23fe5d50deb533ed2d8d2ee43e303ee7bced3626
Dustin McCormick <djmccormick@users.noreply.github.com>
2022-05-30 03:40:34 -0400

    remove unnecessary installs

--- scripts/npm-update-all.sh
+++ scripts/npm-update-all.sh
@@ -4,26 +4,21 @@ ncu -u
 (
 	cd client
 	ncu -u
-	npm install
 )
 (
 	cd common
 	ncu -u
-	npm install
 )
 (
 	cd graphql
 	ncu -u
-	npm install
 )
 (
 	cd migrate
 	ncu -u
-	npm install
 )
 (
 	cd worker
 	ncu -u
-	npm install
 )
 npm install
